bc31e9088613581449d0b45c71190ec0ab1e1510
Paweł Naglik <pawel@subtelnie.com>
2023-11-17 00:22:54 +0100

    Dzień 5 działania na liczbach

--- ChallengApp/ChallengApp/Program.cs
+++ ChallengApp/ChallengApp/Program.cs
@@ -1,35 +1,67 @@
-﻿// zmienne
-using System.ComponentModel.Design;
-
-string name = "Ewa";
-int age = 33;
-char sex = 'k';
-//instrukcje
-if (age >= 120)
+﻿// założenia
+int number = 80222;
+string numberInString = number.ToString();
+char[] dane = numberInString.ToArray();
+var i = 0;
+//tablica wartości
+List<int> liczba = new List<int>();
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+liczba.Add(0);
+//działania na liczbach
+foreach (char dodawanie in dane)
 {
-    Console.WriteLine("Jesteś martwy ;)");
+    if (dodawanie == '0')
+    {
+        liczba[0]++;
+    }
+    else if (dodawanie == '1')
+    {
+        liczba[1]++;
+    }
+    else if (dodawanie == '2')
+    {
+        liczba[2]++;
+    }
+    else if (dodawanie == '3')
+    {
+        liczba[3]++;
+    }
+    else if (dodawanie == '4')
+    {
+        liczba[4]++;
+    }
+    else if (dodawanie == '5')
+    {
+        liczba[5]++;
+    }
+    else if (dodawanie == '6')
+    {
+        liczba[6]++;
+    }
+    else if (dodawanie == '7')
+    {
+        liczba[7]++;
+    }
+    else if (dodawanie == '8')
+    {
+        liczba[8]++;
+    }
+    else if (dodawanie == '9')
+    {
+        liczba[9]++;
+    }
 }
-else if (name == "Ewa" && age == 33 && sex == 'k')
+// Dynamiczne yświetlanie wyniku
+Console.WriteLine("W liczbie " + number);
+foreach (var wyniki in liczba)
 {
-    Console.WriteLine("EWA, lat 33");
-}
-else if (sex == 'k' && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat, dokładnie ma " + age + " lat");
-}
-else if (sex == 'k' && age > 30)
-{
-    Console.WriteLine("Kobieta powyżej 30 lat, dokładnie ma " + age + " lat");
-}
-else if (age < 18 && sex != 'k')
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else if (sex == 'k')
-{
-    Console.WriteLine("Kobieta, " + age + " lat");
-}
-else
-{
-    Console.WriteLine("Mężczyzna, " + age + " lat");
+    Console.WriteLine("cyfra " + i++ + " występuje " + wyniki + " razy");
 }
