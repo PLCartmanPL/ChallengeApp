1bd8580181194ccd18bd223ae94c5750647b4abb
Paweł Naglik <pawel@subtelnie.com>
2023-11-17 00:36:22 +0100

    Poprawka dzień 5 działania na liczbach

--- ChallengApp/ChallengApp/Program.cs
+++ ChallengApp/ChallengApp/Program.cs
@@ -1,67 +1,67 @@
 ﻿// założenia
 int number = 80222;
 string numberInString = number.ToString();
-char[] dane = numberInString.ToArray();
+char[] data = numberInString.ToArray();
 var i = 0;
 //tablica wartości
-List<int> liczba = new List<int>();
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-liczba.Add(0);
-//działania na liczbach
-foreach (char dodawanie in dane)
+List<int> counter = new List<int>();
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+counter.Add(0);
+//działania na counterch
+foreach (char adding in data)
 {
-    if (dodawanie == '0')
+    if (adding == '0')
     {
-        liczba[0]++;
+        counter[0]++;
     }
-    else if (dodawanie == '1')
+    else if (adding == '1')
     {
-        liczba[1]++;
+        counter[1]++;
     }
-    else if (dodawanie == '2')
+    else if (adding == '2')
     {
-        liczba[2]++;
+        counter[2]++;
     }
-    else if (dodawanie == '3')
+    else if (adding == '3')
     {
-        liczba[3]++;
+        counter[3]++;
     }
-    else if (dodawanie == '4')
+    else if (adding == '4')
     {
-        liczba[4]++;
+        counter[4]++;
     }
-    else if (dodawanie == '5')
+    else if (adding == '5')
     {
-        liczba[5]++;
+        counter[5]++;
     }
-    else if (dodawanie == '6')
+    else if (adding == '6')
     {
-        liczba[6]++;
+        counter[6]++;
     }
-    else if (dodawanie == '7')
+    else if (adding == '7')
     {
-        liczba[7]++;
+        counter[7]++;
     }
-    else if (dodawanie == '8')
+    else if (adding == '8')
     {
-        liczba[8]++;
+        counter[8]++;
     }
-    else if (dodawanie == '9')
+    else if (adding == '9')
     {
-        liczba[9]++;
+        counter[9]++;
     }
 }
 // Dynamiczne yświetlanie wyniku
 Console.WriteLine("W liczbie " + number);
-foreach (var wyniki in liczba)
+foreach (var result in counter)
 {
-    Console.WriteLine("cyfra " + i++ + " występuje " + wyniki + " razy");
+    Console.WriteLine("cyfra " + i++ + " występuje " + result + " razy");
 }
