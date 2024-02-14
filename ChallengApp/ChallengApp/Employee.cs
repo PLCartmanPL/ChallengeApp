5d9e2062bea0a7a7e5ff50ebcf656e8b16f70764
Paweł Naglik <pawel@subtelnie.com>
2024-02-14 21:12:24 +0100

    dzień 11 testowe pętle

--- ChallengApp/ChallengApp/Employee.cs
+++ ChallengApp/ChallengApp/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace ChallengApp
+﻿using System;
+
+namespace ChallengApp
 {
     public class Employee
     {
@@ -72,6 +74,65 @@
 
             return statistics;
         }
-        
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics1 = new Statistics();
+
+            statistics1.Average = 0;
+            statistics1.Max = float.MinValue;
+            statistics1.Min = float.MaxValue;
+
+            for (var index = 0; index < this.grades.Count; index++)
+            {
+                statistics1.Max = Math.Max(statistics1.Max, this.grades[index]);
+                statistics1.Min = Math.Min(statistics1.Min, this.grades[index]);
+                statistics1.Average += this.grades[index];
+            }
+
+            statistics1.Average = statistics1.Average / this.grades.Count;
+
+            return statistics1;
+        }
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics2 = new Statistics();
+
+            statistics2.Average = 0;
+            statistics2.Max = float.MinValue;
+            statistics2.Min = float.MaxValue;
+            var index = 0;
+            do
+            {
+                statistics2.Max = Math.Max(statistics2.Max, this.grades[index]);
+                statistics2.Min = Math.Min(statistics2.Min, this.grades[index]);
+                statistics2.Average += this.grades[index];
+                index++;
+            } while (index < this.grades.Count);
+
+            statistics2.Average = statistics2.Average / this.grades.Count;
+
+            return statistics2;
+        }
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics3 = new Statistics();
+
+            statistics3.Average = 0;
+            statistics3.Max = float.MinValue;
+            statistics3.Min = float.MaxValue;
+            var index = 0;
+            while (index < this.grades.Count)
+            {
+                statistics3.Max = Math.Max(statistics3.Max, this.grades[index]);
+                statistics3.Min = Math.Min(statistics3.Min, this.grades[index]);
+                statistics3.Average += this.grades[index];
+                index++;
+            }
+
+            statistics3.Average = statistics3.Average / this.grades.Count;
+
+            return statistics3;
+        }
+
     }
 }
--- ChallengApp/ChallengApp/Program.cs
+++ ChallengApp/ChallengApp/Program.cs
@@ -1,14 +1,29 @@
 ﻿using ChallengApp;
 
 var employee = new Employee("Adam", "Naglik");
-employee.AddGrade("adam");
-employee.AddGrade(1000);
-employee.AddGrade(12);
+
+employee.AddGrade("adm");
 employee.AddGrade(24);
+employee.AddGrade(800);
 employee.AddGrade(11.466);
 
 var statsitic = employee.GetStatistics();
+var statsitic1 = employee.GetStatisticsWithFor();
+var statsitic2 = employee.GetStatisticsWithDoWhile();
+var statsitic3 = employee.GetStatisticsWithWhile();
 
 Console.WriteLine($"Wartość Średnia: {statsitic.Average:N2}");
 Console.WriteLine($"Wartość Minimalna: {statsitic.Min}");
 Console.WriteLine($"Wartość Maksymalna: {statsitic.Max}");
+Console.WriteLine();
+Console.WriteLine($"Wartość Średnia: {statsitic1.Average:N2}");
+Console.WriteLine($"Wartość Minimalna: {statsitic1.Min}");
+Console.WriteLine($"Wartość Maksymalna: {statsitic1.Max}");
+Console.WriteLine();
+Console.WriteLine($"Wartość Średnia: {statsitic2.Average:N2}");
+Console.WriteLine($"Wartość Minimalna: {statsitic2.Min}");
+Console.WriteLine($"Wartość Maksymalna: {statsitic2.Max}");
+Console.WriteLine();
+Console.WriteLine($"Wartość Średnia: {statsitic3.Average:N2}");
+Console.WriteLine($"Wartość Minimalna: {statsitic3.Min}");
+Console.WriteLine($"Wartość Maksymalna: {statsitic3.Max}");
