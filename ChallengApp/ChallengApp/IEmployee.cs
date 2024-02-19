386141fe76934af87c76d628f2534ec9c01db30f
Paweł Naglik <pawel@subtelnie.com>
2024-02-19 12:46:34 +0100

    day 15 - interfejs

---
+++ ChallengApp/ChallengApp/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿namespace ChallengApp
+{
+    public interface IEmployee
+    {
+        public string Name { get; }
+        public string Surname { get; }
+        public char Gender { get; }
+        void AddGrade(float grade);
+        void AddGrade(char grade);
+        void AddGrade(string grade);
+        void AddGrade(double grade);
+        void AddGrade(int grade);
+        Statistics GetStatistics();
+    }
+}
